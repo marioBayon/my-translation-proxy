8010b8f2e503a1f7e3ab22c5dd0849b8db0b5106
Mario Bayón Klein <Mario Bayn Klein>
2017-11-26 16:03:33 +0100

    First commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace my_translation_proxy.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
